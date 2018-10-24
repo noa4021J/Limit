874458a0d3a4c5ab70bae7194ba92b5d55a1e22d
mani0125 <aion1998@yahoo.co.jp>
2018-10-24 17:58:02 +0900

    Initial Commit

---
+++ Limit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Limit
+//
+//  Created by Syunsuke Nakao on 2018/10/24.
+//  Copyright © 2018年 Syunsuke Nakao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
