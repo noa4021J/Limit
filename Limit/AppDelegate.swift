59cf4d81644e5e262f8f19caee44e11582afa532
maniCreate <aion1998@yahoo.co.jp>
2019-06-05 22:25:45 +0900

    Complete final ajustment

--- Limit/AppDelegate.swift
+++ Limit/AppDelegate.swift
@@ -42,54 +42,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         
-        //通知設定がONなら通知を出す
-        //OFFなら通知は出さない
-        
-        if UserDefaults.standard.bool(forKey: "notificationsSwitch") == true {
-            
-            //　通知設定に必要なクラスをインスタンス化
-            let trigger: UNNotificationTrigger
-            let content = UNMutableNotificationContent()
-            var notificationTime = DateComponents()
-            
-            var messageArray:Array = [(date: String, message: String)]()
-            
-            //メッセージデータの取り出し
-            if let loadData = UserDefaults.standard.object(forKey: "messageData") as? [[String:Any]]  {
-                messageArray = loadData.map { (date: $0["date"] as! String, message: $0["message"] as! String) }
-            }
-            
-            //メッセージがあれば、通知を出す
-            //メッセージがまだなければ、通知は出さない
-            if messageArray.isEmpty == false {
-                
-                //通知を出す
-                
-                //通知に表示するメッセージの番号をランダムに決める
-                let num = Int(arc4random_uniform(UInt32(messageArray.count)))
-                
-                //ランダムに決めた番号のメッセージを通知に表示する
-                content.body = messageArray[num].message
-                content.sound = UNNotificationSound.default
-                
-                // トリガー設定
-                notificationTime.hour = 8
-                notificationTime.minute = 0
-                trigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: false)
-                
-                // 通知スタイルを指定
-                let request = UNNotificationRequest(identifier: "id", content: content, trigger: trigger)
-                // 通知をセット
-                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-                
-            } else {
-                //何もしない
-            }
-            
-        } else {
-            //何もしない
-        }
-        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
