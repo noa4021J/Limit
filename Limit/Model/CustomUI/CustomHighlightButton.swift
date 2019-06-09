ebba21fe4b8ea1ec2d18941f2eff92eb9ea11034
maniCreate <aion1998@yahoo.co.jp>
2019-06-09 12:13:50 +0900

    Add sonar-scanner.properties

---
+++ Limit/Model/CustomUI/CustomHighlightButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomHighlightButton.swift
+//  Limit
+//
+//  Created by Syunsuke Nakao on 2019/06/06.
+//  Copyright © 2019 Syunsuke Nakao. All rights reserved.
+//
+
+import UIKit
+
+class CustomHighlightButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
